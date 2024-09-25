52260dcc7e385ab21be87920901730fc644e628a
MrKalisz <72149056+MrKalisz@users.noreply.github.com>
2024-09-24 20:19:18 -0400

    Update PP1_7.py

--- PP1_7.py
+++ PP1_7.py
@@ -1,3 +1,25 @@
 
 
-#Write Assignment code here
+q1():
+  #Write code here
+
+q2():
+  #Write code here
+
+q3():
+  #Write code here
+
+q4():
+  #Write code here
+
+q5():
+  #Write code here
+
+#Do edit the code below
+#Comment the lines below when running your tests
+
+q1()
+q2()
+q3()
+q4()
+q5()
